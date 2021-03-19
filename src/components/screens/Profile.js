14845279afcba0836f060fa9912a8b92dbc6d085
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-03-20 03:01:45 +0700

    Saving update pic

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -5,7 +5,7 @@ const Profile = () => {
   const [myPics, setPics] = useState([]);
   const { state, dispatch } = useContext(UserContext);
   const [image, setImage] = useState("");
-  const [url, setUrl] = useState("");
+  // const [url, setUrl] = useState("");
 
   useEffect(() => {
     fetch("/mypost", {
@@ -32,13 +32,33 @@ const Profile = () => {
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
-          setUrl(data.url);
-          localStorage.setItem(
-            "user",
-            JSON.stringify({ ...state, pic: data.url })
-          );
-          dispatch({ type: "UPDATEPIC", payload: data.url });
+          // console.log(data);
+          // setUrl(data.url);
+          // localStorage.setItem(
+          //   "user",
+          //   JSON.stringify({ ...state, pic: data.url })
+          // );
+          // dispatch({ type: "UPDATEPIC", payload: data.url });
+          fetch("/updatepic", {
+            method: "put",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("jwt"),
+            },
+            body: JSON.stringify({
+              pic: data.url,
+            }),
+          })
+            .then((res) => res.json())
+            .then((result) => {
+              console.log(result);
+              localStorage.setItem(
+                "user",
+                JSON.stringify({ ...state, pic: result.pic })
+              );
+              dispatch({ type: "UPDATEPIC", payload: result.pic });
+            });
+          // window.location.reload();
         })
         .catch((err) => {
           console.log(err);
